35fdab85fd886e1b855bd0bb6b1ea98167ae30a7
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2023-08-10 14:50:21 -0300

    teste

--- src/Exercicios/teste.java
+++ src/Exercicios/teste.java
@@ -1,15 +1,86 @@
 package Exercicios;
 
-public class teste {
-    public static void main(String[] args) {
-        int[] nome = new int[10];
+import java.awt.*;
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-        for (int i = 0; i < 10; i++) {
-            nome[i] = i;
+public class teste {
+    private JLabel timeLabel;
+    private JLabel countdownLabel;
+    private SimpleDateFormat timeFormat;
+    private SimpleDateFormat dateFormat;
+    private Timer timer;
+
+    public teste() {
+        JFrame frame = new JFrame("Smart Watch");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        timeLabel.setForeground(Color.WHITE);
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        countdownLabel = new JLabel();
+        countdownLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        countdownLabel.setForeground(Color.WHITE);
+        countdownLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        frame.setLayout(new GridLayout(3, 1));
+        frame.add(timeLabel);
+        frame.add(countdownLabel);
+        frame.setVisible(true);
+
+        timeFormat = new SimpleDateFormat("HH:mm:ss");
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        timer = new Timer();
+
+        // Cria um TimerTask para atualizar o tempo a cada segundo
+        TimerTask updateTimeTask = new TimerTask() {
+            public void run() {
+                updateTime();
+                updateCountdown();
+            }
+        };
+        timer.scheduleAtFixedRate(updateTimeTask, 0, 1000);
+    }
+
+    private void updateTime() {
+        Date now = new Date();
+        timeLabel.setText(timeFormat.format(now));
+    }
+
+    private void updateCountdown() {
+        Date now = new Date();
+        Date targetTime = new Date();
+        targetTime.setHours(17);
+        targetTime.setMinutes(30);
+        targetTime.setSeconds(0);
+
+        long timeDifferenceMillis = targetTime.getTime() - now.getTime();
+
+        if (timeDifferenceMillis <= 0) {
+            countdownLabel.setText("Já passou das 17:30.");
+            timer.cancel();
+            return;
         }
-        System.out.print(nome[1]);
-        System.out.print(nome[2]);
-        System.out.print(nome[3]);
+
+        long hours = timeDifferenceMillis / (60 * 60 * 1000);
+        long minutes = (timeDifferenceMillis % (60 * 60 * 1000)) / (60 * 1000);
+        long seconds = (timeDifferenceMillis % (60 * 1000)) / 1000;
+
+        countdownLabel.setText("Faltam " + hours + "h " + minutes + "m " + seconds + "s para as 17:30.");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new teste();
+            }
+        });
     }
 }
-
