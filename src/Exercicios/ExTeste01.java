d90eeced35a74a4b9ded35fb02cca59e90a3689d
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2023-08-15 17:28:07 -0300

    Update ExTeste01.java

--- src/Exercicios/ExTeste01.java
+++ src/Exercicios/ExTeste01.java
@@ -1,4 +1,26 @@
 package Exercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ExTeste01 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<String> lista = new ArrayList();
+
+        System.out.println("-------ORGANIZADOR DE NOMES-------");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o primeiro nome: ");
+            lista.add(sc.nextLine());
+        }
+
+
+
+        sc.close();
+    }
 }
